63f8b7ca26b4e911bd69fe56b19afa4faade5621
manideekshith <manideekshithgaming@gmail.com>
2026-08-12 12:30:19 +0530

    harsh chethurvedi aded

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -96,7 +96,7 @@ export function getCurrentUser(): AuthUser | null {
 
 export const isEmployee = (u: AuthUser | null) => !u || u.role === "Employee";
 export const isManager = (u: AuthUser | null) => u?.role === "Manager" || u?.role === "manager" || u?.role === "AGM" || u?.role==="DGM" || u?.role==="Assistant Manager" || u?.role==="Senior Manager" || u?.role==="Head of Department" || u?.role==="Department Head" || u?.role===
-"Deputy Manager" || u?.role==="Director" || u?.role==="head";
+"Deputy Manager" || u?.role==="Director" || u?.role==="head" || u?.role==="Associate Vice President" || u?.role==="Vice President" || u?.role==="AVP" || u?.role==="VP";
 export const isHead = (u: AuthUser | null) => u?.role === "Head" || u?.role==="head" || u?.role === "Director";
 export const isHR = (u: AuthUser | null) =>
   u?.role === "HR" || u?.role === "admin";
